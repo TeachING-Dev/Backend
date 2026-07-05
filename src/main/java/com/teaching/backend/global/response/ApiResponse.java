9c29fdd2c8695cc780b03c4285d859037c8e5fd3
hwiyoon20010309 <hwiyoon01@dgu.ac.kr>
2026-07-05 13:23:36 +0900

    [Chore] 백엔드 프로젝트 초기 세팅

---
+++ src/main/java/com/teaching/backend/global/response/ApiResponse.java
@@ -0,0 +1,35 @@
+package com.teaching.backend.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private Boolean isSuccess;
+    private String code;
+    private String message;
+    private T result;
+    private Object error;
+
+    public static <T> ApiResponse<T> onSuccess(T result) {
+        return new ApiResponse<>(
+                true,
+                "COMMON200",
+                "요청에 성공했습니다.",
+                result,
+                null
+        );
+    }
+
+    public static <T> ApiResponse<T> onFailure(String code, String message, Object error) {
+        return new ApiResponse<>(
+                false,
+                code,
+                message,
+                null,
+                error
+        );
+    }
+}
